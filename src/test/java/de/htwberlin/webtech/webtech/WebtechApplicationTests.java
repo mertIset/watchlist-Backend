d6818425ba70a1b68e0382dd037b3633cc0f0ced
mertiset <mertiset11@gmail.com>
2025-06-06 12:43:26 +0200

    application-test.properties (mit den DB-Credentials) als Ressource für WebtechApplicationTests

--- src/test/java/de/htwberlin/webtech/webtech/WebtechApplicationTests.java
+++ src/test/java/de/htwberlin/webtech/webtech/WebtechApplicationTests.java
@@ -2,12 +2,15 @@ package de.htwberlin.webtech.webtech;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Verwendet application-test.properties
 class WebtechApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Test überprüft, ob der Spring-Kontext erfolgreich geladen wird
 	}
 
 }
